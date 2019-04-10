7ee9d901a622ce9d5bb2ae970ca7e4c953b31b60
Basile Chatillon <basile.chatillon@arcanite.ch>
2019-04-10 12:00:27 +0200

    Update code so the default indent for google docsting is 4

--- pyment/docstring.py
+++ pyment/docstring.py
@@ -1743,7 +1743,7 @@ class DocString(object):
                         raw += ' (Default value = ' + str(p[3]) + ')'
                 raw += '\n'
         elif self.dst.style['out'] == 'google':
-            spaces = ' ' * 2
+            spaces = ' ' * 4
             with_space = lambda s: '\n'.join([self.docs['out']['spaces'] +\
                                                     l.lstrip() if i > 0 else\
                                                     l for i, l in enumerate(s.splitlines())])
@@ -1805,7 +1805,7 @@ class DocString(object):
                 raw += '\n'
                 if 'raise' in self.dst.googledoc.get_mandatory_sections() or \
                         (self.docs['out']['raises'] and 'raise' in self.dst.googledoc.get_optional_sections()):
-                    spaces = ' ' * 2
+                    spaces = ' ' * 4
                     with_space = lambda s: '\n'.join([self.docs['out']['spaces'] + spaces + \
                                                             l.lstrip() if i > 0 else \
                                                             l for i, l in enumerate(s.splitlines())])
@@ -1874,7 +1874,7 @@ class DocString(object):
                 raw += '\n' + self.docs['out']['spaces'] + spaces + with_space(self.docs['out']['return']).strip() + '\n'
         elif self.dst.style['out'] == 'google':
             raw += '\n'
-            spaces = ' ' * 2
+            spaces = ' ' * 4
             with_space = lambda s: '\n'.join([self.docs['out']['spaces'] + spaces +\
                                                     l.lstrip() if i > 0 else\
                                                     l for i, l in enumerate(s.splitlines())])
